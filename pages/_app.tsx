0d9656cc36a7a3208197367eff0ea6c6ca1b363d
nicola-commits <nicolamigone179@gmail.com>
2023-06-17 17:32:58 +0200

    Added boostrapl, better SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.css' // Bootstrap
 import '@/styles/globals.css'
 
 function MyApp({ Component, pageProps }: {Component: any, pageProps: any}) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="viewport-fit=cover width=device-width" />
+        <meta name="viewport" content="viewport-fit=cover width=device-width initial-scale=1" />
         <meta name="description" content="Get information about your teacher for the best grade in your next exam!"/>
-        <meta name="keywords" content="teacher, exam, prepare"/>
+        <meta name="keywords" content="know ur teacher, teacher, exam, prepare, know, ur"/>
         <meta name="author" content="Ilaria Migone"/>
         <meta charSet="UTF-8"/>
       </Head>
