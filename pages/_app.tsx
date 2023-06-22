b2b8f46ccff9b855549faa54d0da2effccf49b6a
nicola-commits <nicolamigone179@gmail.com>
2023-06-22 16:41:20 +0200

    Improved SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }: {Component: any, pageProps: any}) {
       <Head>
         <meta name="viewport" content="viewport-fit=cover width=device-width initial-scale=1" />
         <meta name="description" content="Get information about your teacher for the best grade in your next exam!"/>
-        <meta name="keywords" content="know ur teacher, teacher, exam, prepare, know, ur"/>
+        <meta name="keywords" content="know ur teacher, teacher, exam, prepare, know, ur, school, tutoring"/>
         <meta name="author" content="Ilaria Migone"/>
         <meta charSet="UTF-8"/>
         <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
