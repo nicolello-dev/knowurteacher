5dfdca274f6ae44133f2b4646b28d36cf5f2df47
nicola-commits <nicolamigone179@gmail.com>
2023-06-26 11:33:38 +0200

    Adde title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }: {Component: any, pageProps: any}) {
         <meta name="keywords" content="know ur teacher, teacher, exam, prepare, know, ur, school, tutoring"/>
         <meta name="author" content="Ilaria Migone"/>
         <meta charSet="UTF-8"/>
+        <title>Know ur teacher | Home</title>
         <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
