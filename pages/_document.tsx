03bfdb2a5a2e29e50e2b1dc4576eb592e8a1f923
nicola-commits <nicolamigone179@gmail.com>
2023-06-26 11:12:23 +0200

    Added GTAG

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
+      <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-MRPKR5CZEP"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-MRPKR5CZEP');
+              `,
+            }}
+          />
+      </Head>
       <body>
         <Main />
         <NextScript />
