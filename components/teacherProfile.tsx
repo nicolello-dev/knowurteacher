25616c11b6d52b2da93d08e14f44b4d6cac67835
nicola-commits <nicolamigone179@gmail.com>
2023-06-29 00:09:35 +0200

    Remove unused 0 length children

--- components/teacherProfile.tsx
+++ components/teacherProfile.tsx
@@ -7,9 +7,7 @@ export default function TeacherProfile({ teacher }: {teacher: Teacher}) {
     return (<>
         <div className={styles.teacherWrapper}>
             <div className={styles.imageWrapper}>
-                <Image src={teacher.photoURL || "https://cdn.knowurteacher.com/defaultpfp.png"} alt={`${teacher.name}'s profile picture`} fill={true}>
-
-                </Image>
+                <Image src={teacher.photoURL || "https://cdn.knowurteacher.com/defaultpfp.png"} alt={`${teacher.name}'s profile picture`} fill={true}/>
             </div>
             <p>
                 Name: <b>{teacher.name}</b>
