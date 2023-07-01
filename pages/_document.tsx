94ea8e3de5a7c12c5a08fb57fb079308067984b6
nicola-commits <nicolamigone179@gmail.com>
2023-07-01 22:29:48 +0200

    Added CSP

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,18 @@
+import { randomBytes } from 'crypto'
+
 import { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
 export default function Document() {
+  const nonce = randomBytes(128).toString('base64')
+  const csp = `object-src 'none'; base-uri 'none'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https: http: 'nonce-${nonce}' 'strict-dynamic'`
+
   return (
     <Html lang="en">
-      <Head>
-          <Script src="https://www.googletagmanager.com/gtag/js?id=G-MRPKR5CZEP"/>
-          <Script id="google-analytics">
+      <Head nonce={nonce}>
+        <meta httpEquiv='Content-Security-Policy' content={csp} />
+          <Script nonce={nonce} src="https://www.googletagmanager.com/gtag/js?id=G-MRPKR5CZEP"/>
+          <Script nonce={nonce} id="google-analytics">
             {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
@@ -18,7 +24,7 @@ export default function Document() {
       </Head>
       <body>
         <Main />
-        <NextScript />
+        <NextScript nonce={nonce} />
       </body>
     </Html>
   )
