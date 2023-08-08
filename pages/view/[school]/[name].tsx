511c890fcb9bbfc4cb60174f7a2e06caac6dc210
ilariiiiia <82510639+ilariiiiia@users.noreply.github.com>
2023-08-08 14:45:00 +0000

    Updated text and title

--- pages/view/[school]/[name].tsx
+++ pages/view/[school]/[name].tsx
@@ -8,6 +8,7 @@ import { useSession } from 'next-auth/react';
 import Footer from "@/components/footer";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function ViewTeacherReviews() {
 
@@ -50,6 +51,9 @@ export default function ViewTeacherReviews() {
 	if(loading) {
 		return <>
 			<Header/>
+			<Head>
+				<title>View teacher | Knowurteacher</title>
+			</Head>
 			<h1 className="mx-auto text-center text-xl p-5">Loading, please wait...</h1>
 		</>
 	}
@@ -57,39 +61,45 @@ export default function ViewTeacherReviews() {
     if(!teacher) {
         return (
             <>
-            <Header/>
-			<h1 className="mx-auto text-center text-3xl p-5">
-				The teacher you tried to search for doesn&apos;t exist.
-			</h1>
-			<p className="mx-auto text-center text-xl p-5">
-				If you believe this is an error, please contact support.
-			</p>
+				<Head>
+					<title>View teacher | Knowurteacher</title>
+				</Head>
+	            <Header/>
+				<h1 className="mx-auto text-center text-3xl p-5">
+					The teacher you tried to search for doesn&apos;t exist.
+				</h1>
+				<p className="mx-auto text-center text-xl p-5">
+					If you believe this is an error, please contact support.
+				</p>
             </>
         )
     }
 
     return (
         <>
-        <Header/>
-        <div className="relative px-3 py-3 mb-4 border rounded bg-green-200 border-green-300 text-green-800" role="alert" style={{ display: showSuccess ? "block" : "none" }}>
-            Review added successfully!
-        </div>
-        <div className="relative px-3 py-3 mb-4 border rounded bg-red-200 border-red-300 text-red-800" role="alert" style={{ display: showError ? "block" : "none" }}>
-            {errorMesssage}
-        </div>
-        <div className="flex flex-wrap justify-center items-center m-12">
-            <TeacherSelect teacher={teacher} button={false}/>
-            <div className="flex m-6">
-                <ShowAvgReview reviews={reviews}/>
-            </div>
-        </div>
-        <div className="text-center">
-            <h3 className="m-6 mb-4">
-                Do you know them? Rate them yourself!
-            </h3>
-            <RateTeacher teacher={teacher} session={session} setError={setErrorMessage} setShowSuccess={setShowSuccess} setShowError={setShowError} setReviews={setReviews}/>
-        </div>
-        <Footer/>
+			<Head>
+				<title>View teacher | Knowurteacher</title>
+			</Head>
+	        <Header/>
+	        <div className="relative px-3 py-3 mb-4 border rounded bg-green-200 border-green-300 text-green-800" role="alert" style={{ display: showSuccess ? "block" : "none" }}>
+	            Review added successfully!
+	        </div>
+	        <div className="relative px-3 py-3 mb-4 border rounded bg-red-200 border-red-300 text-red-800" role="alert" style={{ display: showError ? "block" : "none" }}>
+	            {errorMesssage}
+	        </div>
+	        <div className="flex flex-wrap justify-center items-center m-12">
+	            <TeacherSelect teacher={teacher} button={false}/>
+	            <div className="flex m-6">
+	                <ShowAvgReview reviews={reviews}/>
+	            </div>
+	        </div>
+	        <div className="text-center">
+	            <h3 className="m-6 mb-4">
+	                Do you know them? Rate them yourself!
+	            </h3>
+	            <RateTeacher teacher={teacher} session={session} setError={setErrorMessage} setShowSuccess={setShowSuccess} setShowError={setShowError} setReviews={setReviews}/>
+	        </div>
+	        <Footer/>
         </>
     )
 }
