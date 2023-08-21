46eb1edb14869e2a2e6003b1f9c67ab8b7d71eb7
Ilaria <82510639+ilariiiiia@users.noreply.github.com>
2023-08-22 01:30:01 +0200

    Prisma client import is now default

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -2,9 +2,8 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient()
+export default globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
+  globalForPrisma.prisma = globalForPrisma.prisma || new PrismaClient()
 }
