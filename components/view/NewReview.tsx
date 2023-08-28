c9877e5a3156776657bd9c7f0ef81307ea2eaa84
Ilariiiiia <nicolamigone179@gmail.com>
2023-08-28 22:56:59 +0000

    Improved GUI for mobile

--- components/view/NewReview.tsx
+++ components/view/NewReview.tsx
@@ -18,32 +18,34 @@ export default function NewReview({ teacherId } : { teacherId: string }) {
         });
     }
 
-    return <form onSubmit={e => handleOnSubmit(e)} className="flex flex-col gap-6 w-80% max-w-[500px] mx-auto my-16">
-        <h1 className="text-xl text-center font-bold">Do you know this teacher? Review them!</h1>
-        <p className="text-lg text-center">Please describe your teacher in these fields: </p>
-        <label>
-            Teaching:
-        </label>
-        <textarea value={reviewData.teaching} onChange={e => setReviewData(p => {
-            return {...p, teaching: e.target.value}
-        })} className="h-32 p-2">
-        </textarea>
-        <label>
-            Fairness:
-        </label>
-        <textarea value={reviewData.fairness} onChange={e => setReviewData(p => {
-            return {...p, fairness: e.target.value}
-        })} className="h-32 p-2">
-        </textarea>
-        <label>
-            General:
-        </label>
-        <textarea value={reviewData.general} onChange={e => setReviewData(p => {
-            return {...p, general: e.target.value}
-        })} className="h-32 p-2">
-        </textarea>
-        <button type="submit" className="bg-primary p-3 m-6 w-min text-white rounded-xl mx-auto">
-            Submit
-        </button>
-    </form>
+    return <div className="mx-auto flex flex-row justify-center align-center"> 
+            <form onSubmit={e => handleOnSubmit(e)} className="flex flex-col gap-6 w-80% max-w-[500px] my-16 m-5">
+            <h1 className="text-xl text-center font-bold">Do you know this teacher? Review them!</h1>
+            <p className="text-lg text-center">Please describe your teacher in these fields: </p>
+            <label>
+                Teaching:
+            </label>
+            <textarea value={reviewData.teaching} onChange={e => setReviewData(p => {
+                return {...p, teaching: e.target.value}
+            })} className="h-32 p-2">
+            </textarea>
+            <label>
+                Fairness:
+            </label>
+            <textarea value={reviewData.fairness} onChange={e => setReviewData(p => {
+                return {...p, fairness: e.target.value}
+            })} className="h-32 p-2">
+            </textarea>
+            <label>
+                General:
+            </label>
+            <textarea value={reviewData.general} onChange={e => setReviewData(p => {
+                return {...p, general: e.target.value}
+            })} className="h-32 p-2">
+            </textarea>
+            <button type="submit" className="bg-primary p-3 m-6 w-min text-white rounded-xl mx-auto">
+                Submit
+            </button>
+        </form>
+    </div>
 }
