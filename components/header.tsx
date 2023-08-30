8f8715bfefe0258f5c5e086fbe5d297a0faa2164
ilariiiiia <82510639+ilariiiiia@users.noreply.github.com>
2023-08-30 14:43:52 +0000

    Added dark mode to header

--- components/header.tsx
+++ components/header.tsx
@@ -19,31 +19,31 @@ export default function Header() {
     }, [showLabel]);
 
     return (<>
-        <header className="bg-white p-6 flex flex-row justify-between flex-wrap">
+        <header className="bg-white p-6 flex flex-row justify-between flex-wrap dark:bg-primary">
             <Link href="/" className="flex text-decoration-none text-primary items-center">
-                <h1 className="text-xl hidden mr-2 sm:block">
+                <h1 className="text-xl hidden mr-2 sm:block dark:text-darktext">
                     Knowurteacher
                 </h1>
                 <div className="relative h-8 aspect-square">
                     <Image src='https://cdn.knowurteacher.com/logo.svg' alt='logo' fill={true}/>
                 </div>
             </Link>
-            <div className="flex flex-row text-white items-center font-sans">
+            <div className="flex flex-row text-black items-center font-sans dark:text-darktext">
             {
                 session ? <>
-                    <button className="inline-block align-middle text-center select-none font-normal whitespace-no-wrap py-1 px-3 leading-normal no-underline text-black" onClick={() => signOut()}>Log Out</button>
+                    <button className="inline-block align-middle text-center select-none font-normal whitespace-no-wrap py-1 px-3 leading-normal no-underline" onClick={() => signOut()}>Log Out</button>
                 </> : <>
                     {svgs.googleLogo(24, 24)}
-                    <button className="inline-block align-middle text-center select-none font-normal whitespace-no-wrap py-1 px-3 leading-normal no-underline text-black" onClick={() => signIn('google')}>Log In</button>
+                    <button className="inline-block align-middle text-center select-none font-normal whitespace-no-wrap py-1 px-3 leading-normal no-underline" onClick={() => signIn('google')}>Log In</button>
                 </>
             }
             </div>
         </header>
 		<div>
 		  <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-			<path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className="fill-white"></path>
-			<path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" className="fill-white"></path>
-			<path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" className="fill-white"></path>
+			<path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className="fill-white dark:fill-primary"></path>
+			<path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" className="fill-white dark:fill-primary"></path>
+			<path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" className="fill-white dark:fill-primary"></path>
 		  </svg>
 	    </div>
         </>)
