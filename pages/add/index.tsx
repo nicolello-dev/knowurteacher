0a5a47b8f4121cdbbab2178db18470f41aff1300
Ilaria <82510639+ilariiiiia@users.noreply.github.com>
2023-09-08 23:01:16 +0200

    Refactor file for useless features

--- pages/add/index.tsx
+++ pages/add/index.tsx
@@ -1,24 +1,16 @@
 import Image from "next/image";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import { useRouter } from "next/router";
 
 export default function AddTeacher() {
 
     const [name, setName] = useState("");
     const [school, setSchool] = useState("");
-    const [showSuccess, setShowSuccess] = useState<boolean>(false);
-    const [showError, setShowError] = useState<boolean>(false);
-    const [errorMesssage, setErrorMessage] = useState<string>("");
-
-    const router = useRouter();
 
     function addTeacher() {
         if (!name || !school) {
-            setErrorMessage("Please enter required fields (name and school)");
-            setShowError(true);
             return;
         }
         const body = {
@@ -29,34 +21,11 @@ export default function AddTeacher() {
             method: "POST",
             body: JSON.stringify(body)
         })
-            .then(r => r.json())
-            .then(r => {
-                if (r.success) {
-                    setShowSuccess(true);
-                } else {
-                    setErrorMessage(r.message);
-                    setShowError(true);
-                }
-            });
     }
-
-    useEffect(() => {
-        showSuccess && setTimeout(_ => setShowSuccess(false), 3000);
-    }, [showSuccess]);
-
-    useEffect(() => {
-        showError && setTimeout(_ => setShowError(false), 3000);
-    }, [showError]);
 
     return (<>
         <Header />
-        <div className={`relative px-3 py-3 mb-4 bg-green-300 rounded ${showSuccess ? "block" : "hidden"}`} role="alert">
-            Teacher added successfully!
-        </div>
-        <div className={`relative px-3 py-3 mb-4 bg-red-500 rounded ${showSuccess ? "block" : "hidden"}`}>
-            {errorMesssage}
-        </div>
-        <h1 className="text-center m-6 mt-8 text-4xl font-serif dark:text-darktext">
+        <h1 className="text-center m-6 mt-8 text-4xl dark:text-darktext">
             Add a teacher:
         </h1>
         <div className="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300 dark:bg-[#26092a] dark:border-primary" style={{ alignItems: 'center', width: 'min-content', minWidth: '270px', margin: '100px auto' }}>
@@ -68,7 +37,7 @@ export default function AddTeacher() {
                 <input type="text" className="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded" value={name} onChange={(e) => setName(e.target.value)} required={true} />
                 <label className="form-label m-2">School:</label>
                 <input type="text" className="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded" value={school} onChange={(e) => setSchool(e.target.value)} required={true} />
-                <button className="inline-block align-middle text-center select-none font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white m-2 mt-6 bg-primary dark:bg-darkprimary" onClick={_ => addTeacher()}>Add</button>
+                <button className="inline-block align-middle text-center select-none font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline text-white m-2 mt-6 bg-primary dark:bg-darkprimary" onClick={_ => addTeacher()}>Add</button>
             </div>
         </div>
         <Footer />
